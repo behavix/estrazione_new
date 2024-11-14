5332a47476a9edcd1a4a7da08465aa6a4ff8afb9
behavix <144628391+behavix@users.noreply.github.com>
2024-11-14 19:13:32 +0100

    Update connAirtable.js
    tolti console log

--- netlify/functions/utils/connAirtable.js
+++ netlify/functions/utils/connAirtable.js
@@ -2,11 +2,9 @@ const Airtable = require('airtable');
 
 // Function to connect to Airtable, receives the base ID as a parameter
 async function connAirtable(airtableBase) {
-    console.log(process.env.AIRTABLE_TOKEN);
     try {
         // Initialize the connection to Airtable using the credentials saved in the environment variable
         const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(airtableBase);
-        console.log("ciaone arrivederci");
         return base;
     } catch (error) {
         console.error("Errore durante la connessione ad Airtable:", error);
