dfd602ff8204fd0fe96c4954b8f96b2e5fda0083
behavix <144628391+behavix@users.noreply.github.com>
2024-12-06 11:41:22 +0100

    Update generateToken.js
    andiamo a pescare direttamente l'url di netlify dal config.

--- netlify/functions/generateToken.js
+++ netlify/functions/generateToken.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
+const config = require('./config/config.json');
 
 exports.handler = async function(event) {
     // Generate a token
     const secretKey = process.env.JWT_SECRET_KEY;
     const token = jwt.sign({ isValid: true }, secretKey, { expiresIn: '2m' });
-
-    // Obtain current url
-    const host = event.headers.host;
-    const protocol = event.headers['x-forwarded-proto'] || 'http';
-    const origin = `${protocol}://${host}`;
+  
     // Create destination url
+    const origin = config.url;
     const pathname = '/tenta-la-fortuna';
     const redirectUrl = `${origin}${pathname}?token=${token}`;
     
